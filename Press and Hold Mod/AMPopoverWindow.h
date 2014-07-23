8c8cb98626e4b6ea110a7a1457bad71591326da3
amomchilov <alexandermomchilov@gmail.com>
2014-07-23 17:59:29 -0400

    ?

---
+++ Press and Hold Mod/AMPopoverWindow.h
@@ -0,0 +1,11 @@
+//  AMPopover.h
+//  Created by Alexander Momchilov on 2014-07-15.
+//  Copyright (c) 2014 Alexander Momchilov. All rights reserved.
+
+#import <Cocoa/Cocoa.h>
+
+@interface AMPopoverWindow : NSWindow
+
+@property (weak) IBOutlet NSTextField *label;
+
+@end
