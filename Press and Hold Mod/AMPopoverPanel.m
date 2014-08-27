e6eada617eeb5a2108f492fb1d9300d90443098d
amomchilov <alexandermomchilov@gmail.com>
2014-08-27 11:01:40 -0400

    moved positioning logic to AMPopoverPanel, so that it automatically places itself around a given NSView, with the given size

--- Press and Hold Mod/AMPopoverPanel.m
+++ Press and Hold Mod/AMPopoverPanel.m
@@ -18,7 +18,7 @@
 		[self setOpaque:NO];
 		[self setBackgroundColor:[NSColor clearColor]];
 		
-		//[self setBackgroundColor:[NSColor greenColor]];
+		[self setBackgroundColor:[NSColor greenColor]];
 	}
 	return self;
 }
@@ -27,11 +27,56 @@
 
 #pragma mark NSResponder methods
 - (void)cancelOperation:(id)sender {
-	if ([self.delegate respondsToSelector:@selector(windowShouldClose:)]
-		&& [self.delegate windowShouldClose:self]) [self close];
+	if ([self.delegate respondsToSelector:@selector(windowShouldClose:)] &&
+		[self.delegate windowShouldClose:self]) [self close];
 }
 
 #pragma mark other methods
+
+- (void) popoverWithSize:(NSSize) contentSize
+				  onView:(NSView *) sender
+		 backgroundColor:(NSColor *) newViewBackgroundColor
+			 borderColor:(NSColor *) newBorderColor
+			 borderWidth:(int) newBorderWidth
+			cornerRadius:(float) newCornerRadius
+			   arrowSize:(NSSize) newArrowSize
+			   arrowEdge:(NSRectEdge) newArrowEdge
+		   arrowPosition:(float) newArrowPosition {
+
+	NSRect senderRect = sender.actualBounds;
+	CGFloat x = senderRect.origin.x;
+	CGFloat y = senderRect.origin.y;
+	switch (newArrowEdge) {
+		case NSMaxYEdge: //arrow pointing up, box below
+			x += senderRect.size.width/2;
+			break;
+
+		case NSMinYEdge: //arrow pointing down, box above
+			x += senderRect.size.width/2;
+			y += senderRect.size.height;
+			break;
+
+		case NSMaxXEdge: //arrow pointing right, box on the left
+			y += senderRect.size.height/2;
+			break;
+
+		case NSMinXEdge: //arrow pointing left, box on the right
+			x += senderRect.size.width;
+			y += senderRect.size.height/2;
+			break;
+	}
+
+	[self popoverWithFrame: NSMakeRect(x, y, contentSize.width, contentSize.height)
+		   backgroundColor: newViewBackgroundColor
+			   borderColor: newBorderColor
+			   borderWidth: newBorderWidth
+			  cornerRadius: newCornerRadius
+				 arrowSize: newArrowSize
+				 arrowEdge: newArrowEdge
+			 arrowPosition: newArrowPosition];
+}
+
+
 - (void) popoverWithFrame:(NSRect) frame
 		  backgroundColor:(NSColor *) newViewBackgroundColor
 			  borderColor:(NSColor *) newBorderColor
@@ -48,14 +93,14 @@
 	NSRect newContentRect = frame;
 	newContentRect.origin = NSZeroPoint;
 	switch (_arrowEdge = newArrowEdge) {
-		case NSMaxYEdge: //Up Arrow
+		case NSMaxYEdge: //arrow pointing up, box below
 			if (newArrowPosition == -1 || newArrowPosition >= frame.size.width) newArrowPosition = frame.size.width/2; //if the newArrowPosition is -1 or too big, newArrowPosition becomes such that the arrow is centered on its edge
 			frame.size.height += newArrowSize.height; //makes room for the arrow
 			frame.origin.y -= frame.size.height; //shifts the frame below the desired point
 			frame.origin.x -= newArrowPosition; //shifts horizontally so the arrow lines up with the desired point
 			break;
 			
-		case NSMinYEdge: //Down Arrow
+		case NSMinYEdge: //arrow pointing down, box above
 			if (newArrowPosition == -1 || newArrowPosition >= frame.size.width) newArrowPosition = frame.size.width/2; //if the newArrowPosition is -1 or too big, newArrowPosition becomes such that the arrow is centered on its edge
 			newContentRect.origin.y += newArrowSize.height; //shifts the contentRect above the desired point
 			
@@ -63,14 +108,14 @@
 			frame.origin.x -= newArrowPosition; //shifts horizontally so the arrow lines up with the desired point
 			break;
 			
-		case NSMaxXEdge: //Right Arrow
+		case NSMaxXEdge: //arrow pointing right, box on the left
 			if (newArrowPosition == -1 || newArrowPosition >= frame.size.height) newArrowPosition = frame.size.height/2; //if the newArrowPosition is -1 or too big, newArrowPosition becomes such that the arrow is centered on its edge
 			frame.size.width += newArrowSize.height;  //makes room for the arrow
 			frame.origin.x -= frame.size.width; //shifts the frame to the left of the desired point
 			frame.origin.y -= newArrowPosition; //shifts vertically so the arrow lines up with the desired point
 			break;
 			
-		case NSMinXEdge: //Left Arrow
+		case NSMinXEdge: //arrow pointing left, box on the right
 			if (newArrowPosition == -1 || newArrowPosition >= frame.size.height) newArrowPosition = frame.size.height/2; //if the newArrowPosition is -1 or too big, newArrowPosition becomes such that the arrow is centered on its edge
 			newContentRect.origin.x += newArrowSize.height; //shifts the contentRect to the right of the desired point
 			
@@ -79,7 +124,7 @@
 			break;
 	}
 	
-	_contentRect = newContentRect;
+	_popUpRect = newContentRect;
 	_viewBackgroundColor = newViewBackgroundColor;
 	_borderColor = newBorderColor;
 	_borderWidth = newBorderWidth;
